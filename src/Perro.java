76cfaef9690811f83fe7e1bf8fe60c094ae24813
maripachon <pachonmaritza102@gmail.com>
2025-04-21 09:47:06 -0500

    Update Perro.java

--- src/Perro.java
+++ src/Perro.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
 public class Perro {
+    private String placa;
+    private String nombre;
+    private String raza;
+    private int edad;
+    private String tamaño;
+
+    public Perro(String placa, String nombre, String raza, int edad, String tamaño) {
+        this.placa = placa;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = edad;
+        this.tamaño = tamaño;
+    }
+    public int getEdad() {
+        return edad;
+    }
+
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    @Override
+    public String toString() {
+        return "Placa: " + placa + ", Nombre: " + nombre + ", Raza: " + raza +
+                ", Edad: " + edad + ", Tamaño: " + tamaño;
+    }
 }
